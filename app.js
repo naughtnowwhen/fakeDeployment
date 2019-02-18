73f18c05330265a960f30d88cbcf16aa6eca0d71
Peter <naughtnowwhen@gmail.com>
2019-02-18 10:01:50 -0800

    added basic node scaffolding

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let test = 'heroku test';
+const express = require('express');
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response)=>{
+  response.send('hello from node');
+})
+
+app.get('/', (request, response)=>{
+  response.status(200).redirect('/index.html');
+})
+
+app.use('*', (request, response)=> response.send('nothing'));
+
+app.listen(PORT, ()=>{
+  console.log(`listening on port : ${PORT}`);
+});
